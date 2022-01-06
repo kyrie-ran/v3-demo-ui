22a97ac2c4e6f1d4404fd3b0a0cda42fbf1dafa7
chenhaoran <chenhaoran@meijing.com>
2022-01-06 11:27:02 +0800

    文件目录整理

---
+++ src/story/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        hello world
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
+const path = require('path')
+const resolve = filePath => path.join(__dirname,'./',filePath)
 module.exports = {
     outputDir: 'docs',
     publicPath: '/v3-demo-ui/',
+    pages:{
+        index: {
+            entry: resolve('/src/story/main.ts'),
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: '组件示例'
+        }
+    },
+    chainWebpack: config => {
+        config.plugins
+            .delete('prefetch-index')
+            .delete('preload-index');
+        config.resolve.alias
+            .set('story', resolve('story'))
+    }
 }
